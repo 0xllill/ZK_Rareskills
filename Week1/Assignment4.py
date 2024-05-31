740941465469da84b2dac88cdb32c1225c04c1b5
Lilian Cariou <liliancariou@gmail.com>
2024-05-31 13:42:16 +0200

    modif assignment4 => Thanks Ret2Basic

--- Week1/Assignment4.py
+++ Week1/Assignment4.py
@@ -11,28 +11,31 @@ g = 5
 #y = 888   # Example solution
 
 # :( better to ask for g^(2x+8y) input but difficult to enter such a big number
-print('Enter first commit 2x+8y:') 
-input1 = input()
-left_side_1 = pow(g, int(input1), n)
+print('Enter first commit x:') 
+input1 = int(input())
+
 
 # :( better to ask for g^(2x+8y) input but difficult to enter such a big number
-print('Enter second commit 5x+3y:')
-input2 = input()
-left_side_2 = pow(g, int(input2), n)
+print('Enter second commit y:')
+input2 = int(input())
 
+
+left_side_1 = pow(g, int(2 * input1 + 8 * input2), n)
+left_side_2 = pow(g, int(5 * input1 + 3 * input2), n)
+
+
+
+
+###### VERIFIER SIDE
 # Homomorphic proof
 # 2x + 8y = 7944
 right_side_1 = pow(g, 7944, n)
 # 5x + 3y = 4764
 right_side_2 = pow(g, 4764, n)
 
-# Verifier checks
+# Verifier checks 
 print(f"Verify first equation: {left_side_1 == right_side_1}")
 print(f"Verify second equation: {left_side_2 == right_side_2}")
 
-# For demonstration, the verifier can check that discrete logs match
-x_discrete_log = discrete_log(n, left_side_1, g)
-y_discrete_log = discrete_log(n, left_side_2, g)
 
-print(f"Discrete log of C_x: {x_discrete_log == input1}")
-print(f"Discrete log of C_y: {y_discrete_log == input2}")
+
